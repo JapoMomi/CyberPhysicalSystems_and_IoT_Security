cf20c50123eddbbf86b7a2f971be960f6cf473bc
JapoMomi <mom.jacopo@gmail.com>
2024-12-26 17:22:56 +0100

    Almost complete the second version of the simulator

--- bus_off_attack.py
+++ bus_off_attack.py
@@ -19,17 +19,29 @@ VICTIM_ECU = "Victim   "
 NORMAL_ECU = "Normal   "
 ADVERSARY_ECU = "Adversary"
 
+running = True
+
 class Packet:
     def __init__(self, ecu_id, id, rtr, data, control=CONTROL, sof=SOF, ack=ACK, eof=EOF, crc=CRC):
+        # Indicates which ECU sends the packet
         self.ecu_id = ecu_id
-        self.id = id
-        self.rtr = rtr
+        # Start Of Frame --> 0 if the node want to talk to other nodes
+        self.sof = sof # For this project always 1
+        # Frame Identifier --> the lower the higher is the priority (not an identifier of the sender)
+        self.id = id 
+        # Remote Trasmission Request --> indicates whether a node sends data or requests data from another node
+        self.rtr = rtr # For this project: 0 -> requests data , 1 -> send data
+        # Contains the Identifier Extension Bit (IEB) which is dominant 0 for 11 bits ID. 
+        # It also contains the 4 bit Data Length Code (DLC) that specifies the length of the data bytes to be transmitted (0 to 8 bytes)
+        self.control = control # For this project 1 if normal or victim ECU, 0 if adversary ECU
+        # Payload
         self.data = data
-        self.control = control
-        self.sof = sof
-        self.ack = ack
-        self.eof = eof
-        self.crc = crc
+        # Check for data integrity
+        self.crc = crc # For this project always 0
+        # Indicates whether a node has acknowledged and received data correctly
+        self.ack = ack # For this project always 0
+        # End Of Frame
+        self.eof = eof # For this project always 1
 
     def to_Array(self):
         return [self.sof, self.id, self.rtr, self.control, self.data, self.crc, self.ack, self.eof]
@@ -48,9 +60,13 @@ class Packet:
 
 
 class ECU:
-    def __init__(self, ecu_id, attack_mode=NORMAL_ECU):
+
+    def __init__(self, ecu_id, attack_mode=NORMAL_ECU, adv_target=-1):
         
         self.attack_mode = attack_mode
+        # Indicate the target of the adv ECU when attack_mode = Adversary
+        # -1 if attack_mode != Adversary, the ecu_id when attack_mode == Adversary
+        self.adv_target = adv_target
         self.ecu_id = ecu_id
         # If TEC or REC > 127 --> mode = ErrorPassive
         # If TEC and REC return < 128 --> mode = ErrorActive
@@ -63,21 +79,10 @@ class ECU:
         self.retransmission_needed = False
         # All ECUs start in Error Active mode
         self.status = ERROR_ACTIVE
+        
         self.tec_history = []
         self.tec_history.append(self.tec)
         self.sent_packets = {}  # Store all packets sent with their ID as key
-
-    def tec_rec_increment(self):
-        
-        self.tec += 8
-        self.rec += 8
-        self.tec_history.append(self.tec)
-
-    def tec_rec_decrement(self):
-        
-        self.tec -= 1
-        self.rec -= 1
-        self.tec_history.append(self.tec)
 
     def ecu_status_check(self):
         
@@ -117,6 +122,7 @@ class ECU:
         """Controlla il log per messaggi con lo stesso ID, ignorando ritrasmissioni."""
         with canbus.lock:
 
+            #self.ecu_status_check()
             duplicates = [msg for msg in canbus.packet_log if msg.get_Id() == packet_id and msg.get_EcuId() != self.ecu_id]
             
             if len(duplicates) > 0:
@@ -131,25 +137,76 @@ class ECU:
                         #print(f"Packets are different. Retransmitting...")
                         
                         self.retransmission_needed = True
-                        #self.ecu_status_check()
 
                         if self.status == ERROR_ACTIVE:
-                            self.active_flag = True
-                            self.tec_rec_increment()
-                        elif self.status == ERROR_PASSIVE:
-                            self.active_flag = False
-                            if self.attack_mode != ADVERSARY_ECU:
-                                self.passive_flag = True
-                                self.tec_rec_increment()
-                                self.tec_rec_decrement()
+
+                            if self.attack_mode == ADVERSARY_ECU:
+                                # Find the victim ecu in case self is an adversary ECU
+                                victim_ecu = next((ecu for ecu in canbus.ecus if ecu.ecu_id == self.adv_target), None)
+
+                                if victim_ecu.status == ERROR_ACTIVE:
+
+                                    self.active_flag = True
+                                    
+                            # Case ECUs are not adversaries    
                             else:
-                                self.tec_rec_decrement()
-                    #else:
-                        #print(f"Packets are identical. No retransmission needed.")
+
+                                self.active_flag = True
+                        
+                        elif self.status == ERROR_PASSIVE:
+                            
+                            if self.attack_mode != ADVERSARY_ECU:
+
+                                self.passive_flag = True
                         
             else:
-                #print(f"ECU {self.ecu_id} found no duplicate for ID: {packet_id}. No retransmission needed.")
+                
                 self.retransmission_needed = False
+
+    def tec_rec_check(self, packet_id, canbus):
+        
+        with canbus.lock:
+
+            self.ecu_status_check()
+            duplicates = [msg for msg in canbus.packet_log if msg.get_Id() == packet_id and msg.get_EcuId() != self.ecu_id]
+            
+            if len(duplicates) > 0:
+                #print(f"ECU {self.ecu_id} detected duplicate ID: {packet_id}. Comparing packets...")
+                
+                # Confronta i pacchetti duplicati
+                for duplicate in duplicates:
+                    
+                    original_data, original_packet = self.sent_packets[packet_id]
+                    # Confronta gli array dei pacchetti
+                    if duplicate.to_Array() != original_packet.to_Array():
+
+                        if self.status == ERROR_ACTIVE and self.active_flag:
+                            
+                            self.tec += 8
+                            self.tec_history.append(self.tec)
+                            self.rec += 8
+
+                        elif self.status == ERROR_PASSIVE and self.passive_flag:
+
+                            self.tec += 8
+                            self.tec_history.append(self.tec)
+                            self.tec -= 1
+                            self.tec_history.append(self.tec)
+                            self.rec += 8
+                            self.rec -= 1
+                        
+                        if self.attack_mode == ADVERSARY_ECU:
+                            victim_ecu = next((ecu for ecu in canbus.ecus if ecu.ecu_id == self.adv_target), None)
+                            if victim_ecu.passive_flag:
+
+                                self.tec -= 1
+                                self.tec_history.append(self.tec)
+                                self.rec -= 1
+
+            self.ecu_status_check()
+            print(f"{self.attack_mode} -> tec: {self.tec} rec: {self.rec} activeFlag: {self.active_flag} passiveFlag: {self.passive_flag} {self.status}")
+            self.active_flag = False
+            self.passive_flag = False
 
 
 class CANBus:
@@ -179,7 +236,9 @@ class CANBus:
 # Funzione per inviare e controllare i messaggi in modo sincronizzato
 def run_ecu(ecu, packet_id, data, canbus, barrier):
     
-    while ecu in canbus.ecus and len(canbus.ecus) != 1:
+    global running
+    while running and ecu in canbus.ecus and len(canbus.ecus) != 1:
+        
         canbus.packet_log.clear()
         
         # Check the status of the ECU
@@ -187,6 +246,7 @@ def run_ecu(ecu, packet_id, data, canbus, barrier):
         # If the ECU is in a bus-off state --> disconnect it from the can bus
         if ecu.status == BUSS_OFF:
             canbus.disconnect_ecu(ecu)
+            running = False
             break
 
         # Sincronizzazione prima dell'invio
@@ -201,15 +261,24 @@ def run_ecu(ecu, packet_id, data, canbus, barrier):
 
         # Sincronizzazione prima del controllo
         barrier.wait()
-        print("-------------------------------------------------------------------")
+
         ecu.check_duplicate_ids(packet_id, canbus)
 
+        barrier.wait()
+
+        ecu.tec_rec_check(packet_id, canbus)
+        
+        print("----------------------------------------------------------------")
         # Aspetta l'intervallo
         time.sleep(1)
 
+
 def plot_tec_history(*ecus):
     for ecu in ecus:
-        plt.plot(ecu.tec_history, label=f"ECU {ecu.ecu_id} ({ecu.attack_mode})")
+        if ecu.attack_mode == ADVERSARY_ECU:
+            plt.plot(ecu.tec_history, label=f"ECU {ecu.ecu_id} ({ecu.attack_mode})", linestyle='--') 
+        else:
+            plt.plot(ecu.tec_history, label=f"ECU {ecu.ecu_id} ({ecu.attack_mode})") 
     plt.xlabel("Time (iterations)")
     plt.ylabel("Transmit Error Counter (TEC)")
     plt.title("TEC Evolution of ECUs")
@@ -217,10 +286,11 @@ def plot_tec_history(*ecus):
     plt.grid()
     plt.show()
 
+
 # Esegui il CAN bus e le ECU
 canbus = CANBus()
 ecu1 = ECU(1)
-ecu2 = ECU(2, attack_mode=ADVERSARY_ECU)
+ecu2 = ECU(2, attack_mode=ADVERSARY_ECU, adv_target=1)
 
 canbus.connect_ecu(ecu1)
 canbus.connect_ecu(ecu2)
